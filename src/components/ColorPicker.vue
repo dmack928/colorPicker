d7a366af463da5d44e42c69bfe6b9af84c7dcabb
Danny Mackey <dmack928@gmail.com>
2019-04-16 18:39:40 -0400

    added more colors

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,73 @@
+<template lang="html">
+  <div>
+    <Pixel
+      isInColorPicker
+      color="white"
+      :current="color == 'white' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="lightblue"
+      :current="color == 'lightblue' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="blue"
+      :current="color == 'blue' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="darkblue"
+      :current="color == 'darkblue' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="red"
+      :current="color == 'red' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="black"
+      :current="color == 'black' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="yellow"
+      :current="color == 'yellow' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="magenta"
+      :current="color == 'magenta' ? true : false"
+    />
+    <Pixel
+      isInColorPicker
+      color="green"
+      :current="color == 'green' ? true : false"
+    />
+
+
+  </div>
+
+</template>
+
+<script>
+import Pixel from "./Pixel";
+
+export default {
+  name: "ColorPicker",
+  props: {
+    color: String
+  },
+  methods: {
+    changeColor: function(color) {
+      alert(color);
+    }
+  },
+  components: {
+    Pixel
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/components/Pixel.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <div
+    :class="['pixel', color, current ? 'current' : '']"
+    @click="handleClick"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "Pixel",
+  props: {
+    color: String,
+    current: Boolean,
+    isInColorPicker: Boolean,
+    isInCanvas: Boolean,
+    index: Number
+  },
+  methods: {
+    handleClick: function() {
+      if (this.isInColorPicker) {
+        this.$root.$emit("updatecolor", this.color);
+      }
+      if (this.isInCanvas) {
+        this.$root.$emit("clickedpixel", this.index);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.pixel {
+  border: 1px solid lightgray;
+  width: 30px;
+  height: 30px;
+  box-sizing:border-box;
+}
+
+.white {
+  background-color: white;
+}
+.lightblue {
+  background-color: rgb(0, 188, 212);
+}
+.blue {
+  background-color: rgb(3, 169, 244);
+}
+.darkblue {
+  background-color: rgb(33, 150, 243);
+}
+
+.red {
+  background-color: rgb(255, 0, 0);
+}
+
+.black {
+  background-color: rgb(0,0,0);
+}
+
+.yellow {
+  background-color: rgb(255,255,0)
+}
+
+.magenta {
+  background-color: rgb(255,0,255)
+}
+
+.green {
+  background-color: rgb(0,128,0)
+}
+.pixel.current {
+  border: 4px solid yellow;
+}
+</style>
